dcaf0d90d63f5e8ba70f44d894161a9911b7e5e6
HistidineDwarf <dvorakdwarf@gmail.com>
2023-10-23 18:33:29 -0700

    school work

--- src/main.py
+++ src/main.py
@@ -1,23 +1,34 @@
 import os
 from datetime import date
 import shutil
+import yaml
 
 GENERATION_DIR = "generations/"
 
-#Next takes one and only part of iterator we care about
-#[1] is the dir names
-# print(len(next(os.walk(GENERATION_DIR))[1]))
+def create_dir() -> str:
+    length = 0
+    for _, dirnames, _ in os.walk(GENERATION_DIR):
+        length = len(dirnames)
 
-length = 0
-for _, dirnames, _ in os.walk(GENERATION_DIR):
-    length = len(dirnames)
+        #Only care about first
+        break
 
-    #Only care about first
-    break
+    dir_name = os.path.join(GENERATION_DIR, str(date.today()) + " G" + str(length))
+    os.mkdir(dir_name)
 
-# print(length)
+    return dir_name
 
-name = os.path.join(GENERATION_DIR, str(date.today()) + " G" + str(length))
-os.mkdir(name)
-pkglist_dir = os.path.join(name, "pkglist.txt")
-shutil.copyfile("pkglist.txt", pkglist_dir)
+def copy_pkglist(dir_name: str):
+    pkglist_dir = os.path.join(dir_name, "pkglist.txt")
+    shutil.copyfile("pkglist.txt", pkglist_dir)
+
+def copy_configs(dir_name):
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+        # print(config['tracked_files'])
+
+dir_name = create_dir()
+copy_pkglist(dir_name)
+copy_configs(dir_name)
+
+
