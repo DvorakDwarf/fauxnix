c3d06798518adf0eaee05c4503526c288c2b1652
Vasya Pupkin <dvorakdwarf@gmail.com>
2023-10-23 20:10:25 -0700

    Starting revert process

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ import os
 from datetime import date
 import shutil
 import yaml
+import argparse
+import subprocess
 
-GENERATION_DIR = "generations/"
+GENERATION_DIR = "src/generations/"
 
 def create_dir() -> str:
     length = 0
     for _, dirnames, _ in os.walk(GENERATION_DIR):
         length = len(dirnames)
 
-        #Only care about first
+        #Only care about first, next wasn't working, idk
         break
 
     dir_name = os.path.join(GENERATION_DIR, str(date.today()) + " G" + str(length))
@@ -20,15 +22,50 @@ def create_dir() -> str:
 
 def copy_pkglist(dir_name: str):
     pkglist_dir = os.path.join(dir_name, "pkglist.txt")
-    shutil.copyfile("pkglist.txt", pkglist_dir)
+    shutil.copyfile("src/pkglist.txt", pkglist_dir)
 
+#TODO
 def copy_configs(dir_name):
-    with open('config.yml', 'r') as file:
+    with open('src/config.yml', 'r') as file:
         config = yaml.safe_load(file)
+        for file in config:
+            config_path = os.path.join(dir_name)
+            shutil.copyfile("src/pkglist.txt", pkglist_dir)
+
         # print(config['tracked_files'])
 
-dir_name = create_dir()
-copy_pkglist(dir_name)
-copy_configs(dir_name)
+parser = argparse.ArgumentParser(
+                prog='fauxnix',
+                description='Declarative configuration for the people, not sysadmins '
+                )
 
+mxgroup = parser.add_mutually_exclusive_group()
 
+mxgroup.add_argument('-s', '--sync', action='store_true',
+                    help="create new generation") 
+mxgroup.add_argument('-r', '--revert', type=int,
+                    help="revert to previous generation. Takes generation #") 
+
+args = parser.parse_args()
+
+if args.sync == True:
+    dir_name = create_dir()
+    copy_pkglist(dir_name)
+    copy_configs(dir_name)
+
+elif args.revert != "":
+    dirs = os.listdir(GENERATION_DIR)
+    revert_dir = ""
+    for dir in dirs:
+        split = dir.split("G")
+        #Everything after G
+        if int(split[1]) == args.revert:
+            revert_dir = dir
+
+    revert_dir = os.path.join(GENERATION_DIR, revert_dir)
+
+    with open('src/config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+        revert_command = config["update_command"] + revert_dir
+        print(revert_command)
+        subprocess.Popen(revert_command)
