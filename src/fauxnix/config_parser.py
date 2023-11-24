2c45106550bf77b57edeb5a126d99d689d38f904
Vasya Pupkin <dvorakdwarf@gmail.com>
2023-11-23 18:57:16 -0800

    You won't believe it, should fix bug

--- src/fauxnix/config_parser.py
+++ src/fauxnix/config_parser.py
@@ -5,27 +5,19 @@ DIRNAME = os.path.dirname(__file__)
 POSSIBLE_PATHS = [
     os.path.join(os.environ["HOME"], ".config/fauxnix/fauxnix.yaml"),
     os.path.join(os.environ["HOME"], ".config/fauxnix/config.yaml"),
-    os.path.join(DIRNAME, "fauxnix.yaml"), #local
     "/etc/fauxnix.yaml",
+    os.path.join(DIRNAME, "fauxnix.yaml"), #local
 ]
-
-print(POSSIBLE_PATHS)
 
 def find_config() -> str:
     for path in POSSIBLE_PATHS:
         if os.path.exists(path):
-            if path == "/etc/fauxnix.yaml":
-                print("Using default config in /etc/fauxnix.yaml")
-                print("Please use fauxnix --init")
-
             return path
-
-CONFIG_PATH = find_config()
 
 #Why have references if you can't concat them ????
 #Change return type to correct thing later
 def load_config(yaml: ruamel.yaml.YAML) -> dict:  
-    active_config = CONFIG_PATH
+    active_config = find_config()
 
     #If user is root, find first available home user
     #If you have multiple users using fauxnix, you are screwed
@@ -41,14 +33,13 @@ def load_config(yaml: ruamel.yaml.YAML) -> dict:
     return config
 
 def dump_config(yaml: ruamel.yaml.YAML, config: dict):
-    active_config = CONFIG_PATH
+    active_config = find_config()
 
     #If user is root, find first available home user
     #If you have multiple active users with fauxnix, you are screwed
     if os.getuid() == 0:
         active_config = find_home()
 
-    print(active_config)
     with open(active_config, 'w') as file:
         yaml.dump(config, file)
 
@@ -62,4 +53,4 @@ def find_home() -> str:
             return fauxnix_config
 
     # print("Could not find config, defaulting")
-    return CONFIG_PATH
+    return find_config()
